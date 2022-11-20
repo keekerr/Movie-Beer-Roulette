8c433f17e60b24cf8ba27b89e0f6299ecf6e9683
Josh Lemmond <joshualemmond@gmail.com>
2022-11-20 16:01:21 -0500

    Link to where you can watch the movie has been added, also added the main genre of the movie

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,47 +1,37 @@
-// image link - use poster_path
-
 var movieTitle = $('.movieTitle');
 var moviePoster = $('.moviePoster')
+var movieDescription = $('.movieDescription');
+var movieLink = $('.movieLink');
+var movieGenres = $('.movieGenres');
+var apiKey = "a704608e266b5b21760a7bf37c54c312";
 
+// getRandomInt is used to generate a random number, we're using it both to give us a random page, and random object from that page (this selects the actual movie that's displayed.)
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
-
-
-
-var theMovieDb = {
-    apiKey: "a704608e266b5b21760a7bf37c54c312",
-    defaultUrl: "https://api.themoviedb.org/3/",
-    timeout: 5000,
-    language: "en-US"
-}
-// fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${theMovieDb.apiKey}&language=en-US&region=US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${getRandomInt(500)}&with_genres=Action&with_watch_monetization_types=flatrate`)
-//     .then(function (response) {
-//         return response.json();
-//     })
-//     .then(function (data) {
-//         console.log(data)
-//         console.log(data.results[getRandomInt(19)])
-//         document.getElementById("title").innerHTML +=
-//         "  " + data.poster_path;
-//         }); 
-
+    
 
 function getMovie() {
-    var movieQuery = `https://api.themoviedb.org/3/discover/movie?api_key=${theMovieDb.apiKey}&language=en-US&region=US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${getRandomInt(500)}&with_genres=Action&with_watch_monetization_types=flatrate`;
+    var movieQuery = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=en-US&region=US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${getRandomInt(500)}&with_genres=Action&with_watch_monetization_types=flatrate`;
     movieTitle.empty();
+    movieDescription.empty();
     $.ajax({
         url: movieQuery,
         method: 'GET',
     }).then(function (response){
         var movieRespone = response.results[getRandomInt(19)];
         var posterImage = `https://image.tmdb.org/t/p/original/${movieRespone.poster_path}`;
+        var whereToWatch = `https://www.themoviedb.org/movie/${movieRespone.id}${movieRespone.original_title}/watch`
+        fetch(`https://api.themoviedb.org/3/movie/${movieRespone.id}?api_key=${apiKey}&language=en-US`).then(response => response.json()).then(function (data) {movieGenres.append(data.genres[0].name)});
         console.log(movieRespone);
         movieTitle.text(`Here is your movie: ${movieRespone.original_title}`);
-        moviePoster.attr('src', posterImage)
+        moviePoster.attr('src', posterImage);
+        movieLink.attr('href', whereToWatch);
+        $('.result-container').append(movieLink);
+        movieDescription.text(`Movie Description: ${movieRespone.overview}`);
         console.log(movieRespone.original_title)
+        console.log(movieRespone.overview);
     })
-
 }
 
 getMovie();
