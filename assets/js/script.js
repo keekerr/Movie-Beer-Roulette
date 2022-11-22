8ea2e34e52d28e5e3086a68ed0ea587bb9d15fce
Josh Lemmond <joshualemmond@gmail.com>
2022-11-22 18:23:35 -0500

    commiting changes before pulling another branch

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@ var movieDescription = $('.movieDescription');
 var movieLink = $('.movieLink');
 var movieGenres = $('.movieGenres');
 var apiKey = "a704608e266b5b21760a7bf37c54c312";
+var randomYear;
 
 // getRandomInt is used to generate a random number, we're using it both to give us a random page, and random object from that page (this selects the actual movie that's displayed.) -JL
 // Source for getRandomInt: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random -JL
@@ -19,6 +20,7 @@ function randomIntFromInterval(min, max) { // min and max included
 //     var movieQuery = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=en-US&region=US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${getRandomInt(500)}&with_genres=Action&with_watch_monetization_types=flatrate`;
 //     movieTitle.empty();
 //     movieDescription.empty();
+//     movieGenres.empty();
 //     $.ajax({
 //         url: movieQuery,
 //         method: 'GET',
@@ -26,7 +28,7 @@ function randomIntFromInterval(min, max) { // min and max included
 //         var movieRespone = response.results[getRandomInt(19)];
 //         var posterImage = `https://image.tmdb.org/t/p/original/${movieRespone.poster_path}`;
 //         var whereToWatch = `https://www.themoviedb.org/movie/${movieRespone.id}${movieRespone.original_title}/watch`
-//         fetch(`https://api.themoviedb.org/3/movie/${movieRespone.id}?api_key=${apiKey}&language=en-US`).then(response => response.json()).then(function (data) {movieGenres.append(data.genres[0].name)});
+//         fetch(`https://api.themoviedb.org/3/movie/${movieRespone.id}?api_key=${apiKey}&language=en-US`).then(response => response.json()).then(function (data) {movieGenres.append('Genre: ' + data.genres[0].name)});
 //         console.log(movieRespone);
 //         movieTitle.text(`Here is your movie: ${movieRespone.original_title}`);
 //         moviePoster.attr('src', posterImage);
@@ -40,13 +42,11 @@ function randomIntFromInterval(min, max) { // min and max included
 
 // getMovie();
 
-var randomYear = randomIntFromInterval(1970, 1979);
-// console.log(randomYear);
-
 function getMovieByYear() {
     var movieQuery = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=en-US&region=US&include_adult=false&include_video=false&page=1&year=${randomYear}`;
     movieTitle.empty();
     movieDescription.empty();
+    movieGenres.empty();
     $.ajax({
         url: movieQuery,
         method: 'GET',
@@ -54,13 +54,15 @@ function getMovieByYear() {
         var movieRespone = response.results[getRandomInt(19)];
         var posterImage = `https://image.tmdb.org/t/p/original/${movieRespone.poster_path}`;
         var whereToWatch = `https://www.themoviedb.org/movie/${movieRespone.id}${movieRespone.original_title}/watch`
-        fetch(`https://api.themoviedb.org/3/movie/${movieRespone.id}?api_key=${apiKey}&language=en-US`).then(response => response.json()).then(function (data) {movieGenres.append(data.genres[0].name)});
+        fetch(`https://api.themoviedb.org/3/movie/${movieRespone.id}?api_key=${apiKey}&language=en-US`).then(response => response.json()).then(function (data) {movieGenres.append('Genre: ' + data.genres[0].name)});
         movieTitle.text(`Here is your movie: ${movieRespone.original_title}`);
         moviePoster.attr('src', posterImage);
+        movieLink.text('Click here to see where you can watch this movie!')
         movieLink.attr('href', whereToWatch);
         $('.result-container').append(movieLink);
         movieDescription.text(`Movie Description: ${movieRespone.overview}`);
         console.log(movieRespone.original_title)
+        console.log(movieRespone);
     })
 }
 
